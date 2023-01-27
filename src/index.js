bccf72dc9a856683e9a5433a37e561e61ee57499
David Watson <dwatson@gogoair.com>
2023-01-27 13:12:16 -0500

    typing

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const users = [];
+
+io.on("connection", (socket) => {
+  socket.on("new user", ({ username, userId }) => {
+    users.push({ username, userId });
+    socket.broadcast.emit("joined chat", username);
   });
 
-
-  const users = [];
-
-  io.on('connection', (socket) => {
-    socket.on('new user', ({username, userId}) => {
-        users.push({username, userId});
-        socket.broadcast.emit('joined chat', username);
-      });
-
-    socket.on('typing', id => {
-      console.log('TYPING!', id)
-      const user = users.find(user => user.userId === id);
-      socket.broadcast.emit('user typing', `${user.username} is typing...`);
-    })
-
-    socket.on('chat message', (message) => {
-        io.emit('chat', socket.id, message);
-    });
+  socket.on("typing", (id) => {
+    const user = users.find((user) => user.userId === id);
+    socket.broadcast.emit("user typing", `${user.username}`);
   });
+
+  socket.on("chat message", (id, message) => {
+    const { username } = users.find((user) => user.userId === id);
+    io.emit("chat", username, message);
+  });
+});
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
